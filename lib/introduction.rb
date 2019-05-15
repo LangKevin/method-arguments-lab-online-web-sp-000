7f6dc7af7ac3148e115566ee1a681af9b5445eaa
kevin lang <langkevina@gmail.com>
2019-05-15 23:17:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,6 @@
 def introduction(name)
+# xpect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
   puts "Hi, my name is #{name}."
 end;
+
+# expect{introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n"
